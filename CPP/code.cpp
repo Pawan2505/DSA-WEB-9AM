d2a2c1e8b0e48df7e77e74cd202b2ccdb94ba72c
pawan2505 <pawanaktu@gmail.com>
2025-01-03 10:30:50 +0530

    Binary Search

--- CPP/code.cpp
+++ CPP/code.cpp
@@ -5392,38 +5392,86 @@
 // }
 
 
+// #include<iostream>
+// using namespace std;
+
+// int linearSearch(int arr[], int n, int key){
+
+//     for(int i = 0; i < n; i++){
+
+//         if(arr[i] == key){
+
+//             int index = i;
+//             return index;
+//         }
+//     }
+
+//     return -1;
+
+// }
+
+// int main(){
+
+//     int n = 10;
+
+//     int arr[n] = {10,20,30,40,50,60,70,80,90,100};
+
+//     int key = 60;
+
+//     int result =  linearSearch(arr,n, key);
+
+//     if(result != -1){
+//         cout<<"Element is present at index : "<<result<<endl;
+//     }else{
+//         cout<<"Element not present in array !"<<endl;
+//     }
+
+// }
+
+
+
 #include<iostream>
 using namespace std;
 
-int linearSearch(int arr[], int n, int key){
+int binarySearch(int arr[], int n, int key){
+   
+    int start = 0;
+    int end = 9;
 
-    for(int i = 0; i < n; i++){
+    while(start <= end){
 
-        if(arr[i] == key){
+        int mid  = (start+end)/2;
 
-            int index = i;
-            return index;
+        if(arr[mid] == key){
+            return mid;
+        }
+        else if(arr[mid] < key){
+            start = mid+1;
+        }
+        else{
+            end = mid-1;
         }
     }
 
     return -1;
-
 }
+
 
 int main(){
 
-    int n = 10;
+    int n =10; 
 
     int arr[n] = {10,20,30,40,50,60,70,80,90,100};
 
     int key = 60;
 
-    int result =  linearSearch(arr,n, key);
+  int result = binarySearch(arr,n, key);
 
-    if(result != -1){
-        cout<<"Element is present at index : "<<result<<endl;
-    }else{
-        cout<<"Element not present in array !"<<endl;
-    }
+  if(result != -1){
+    cout<<"Array element present at index : "<<result<<endl;
+  }else{
+    cout<<"Element not exist!"<<endl;
+  }
 
+ 
 }
