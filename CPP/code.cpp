4a6393ba27cf1de3ad26b4b2543eb2bafe8217e0
pawan2505 <pawanaktu@gmail.com>
2025-01-09 10:10:47 +0530

    code

--- CPP/code.cpp
+++ CPP/code.cpp
@@ -5430,48 +5430,80 @@
 
 
 
+// #include<iostream>
+// using namespace std;
+
+// int binarySearch(int arr[], int n, int key){
+   
+//     int start = 0;
+//     int end = 9;
+
+//     while(start <= end){
+
+//         int mid  = (start+end)/2;
+
+//         if(arr[mid] == key){
+//             return mid;
+//         }
+//         else if(arr[mid] < key){
+//             start = mid+1;
+//         }
+//         else{
+//             end = mid-1;
+//         }
+//     }
+
+//     return -1;
+// }
+
+
+// int main(){
+
+//     int n =10; 
+
+//     int arr[n] = {10,20,30,40,50,60,70,80,90,100};
+
+//     int key = 60;
+
+//   int result = binarySearch(arr,n, key);
+
+//   if(result != -1){
+//     cout<<"Array element present at index : "<<result<<endl;
+//   }else{
+//     cout<<"Element not exist!"<<endl;
+//   }
+
+ 
+// }
+
+
+// Sorting
+
 #include<iostream>
 using namespace std;
 
-int binarySearch(int arr[], int n, int key){
-   
-    int start = 0;
-    int end = 9;
+int bubbleSort(int arr[], int n){
 
-    while(start <= end){
+    for(int i = 0; i < n-1; i++){
 
-        int mid  = (start+end)/2;
+        for(int j = 0; j < n-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+            }
+        }
 
-        if(arr[mid] == key){
-            return mid;
-        }
-        else if(arr[mid] < key){
-            start = mid+1;
-        }
-        else{
-            end = mid-1;
-        }
     }
 
-    return -1;
 }
-
 
 int main(){
 
-    int n =10; 
+    int n = 7;
+    int arr[n] = {18,7,99,77,22,9,10};
 
-    int arr[n] = {10,20,30,40,50,60,70,80,90,100};
+    bubbleSort(arr,n);
 
-    int key = 60;
-
-  int result = binarySearch(arr,n, key);
-
-  if(result != -1){
-    cout<<"Array element present at index : "<<result<<endl;
-  }else{
-    cout<<"Element not exist!"<<endl;
-  }
-
- 
+    for(int i = 0; i < n; i++){
+        cout<<arr[i]<<" ";
+    }
 }
